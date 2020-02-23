b936ae3d6fc811c815f10aeb421f4a674e35a09d
AxelGarciaTello <gata21022000@hotmail.com>
2020-02-23 17:13:23 -0600

    Metodo main del programa

---
+++ Main.c
@@ -0,0 +1,21 @@
+/*
+Instituto Politecnico Nacional
+Escuela Superior de Cómputo
+Analisis de algoritmos
+Grupo: 3CV2
+Alumnos: Garcia Tello Axel
+		 Rodríguez Acosta Alan
+Profesor: Benjamin Luna Benoso
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Numeros perfectos
+
+Fecha: 12 de febrero de 2020
+*/
+#include "NumerosPerfectos.h"
+
+int main(void){
+	int numero;
+	printf("¿Cuántos numeros perfectos quieres?\n");
+	scanf("%d",&numero);
+	imprimirNumeroPerfecto(numero);
+}
