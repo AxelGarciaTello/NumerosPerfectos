b2c04857c279a1ea7100fb12624d5d41fd147564
AxelGarciaTello <gata21022000@hotmail.com>
2020-02-23 17:14:36 -0600

    Cabeceras de las funciones de números perfectos

---
+++ NumerosPerfectos.h
@@ -0,0 +1,18 @@
+/*
+Instituto Politecnico Nacional
+Escuela Superior de Cómputo
+Analisis de algoritmos
+Grupo: 3CV2
+Alumnos: Garcia Tello Axel
+		 Rodríguez Acosta Alan
+Profesor: Benjamin Luna Benoso
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Numeros perfectos
+
+Fecha: 12 de febrero de 2020
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int verificarNumeroPerfecto(int numero);
+void imprimirNumeroPerfecto(int cantidad);
